a741887ec0a057c097796c5cd05958e239ff6556
Aaron Sadiq <aaronsadiq284@gmail.com>
2024-06-10 11:13:36 +0500

    latest work

---
+++ src/js/function.js
@@ -0,0 +1,136 @@
+jQuery(function($) {
+    "use strict";
+    AOS.init({
+        duration: 1200,
+    })
+    $(function($) {
+        $(".menu-btn").on('click', function() {
+            $("body").toggleClass("menu-active");
+        });
+    });
+    $(function() {
+        var owl = $("#main-slider");
+        owl.owlCarousel({
+            items: 1,
+            loop: true,
+            nav: false,
+            autoplay: false,
+            dots: true,
+            smartSpeed: 3000,
+            animateOut: 'fadeOut',
+            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+            responsiveClass:true,
+            responsive:{
+                992:{
+                    nav: true,
+                },
+            }
+        });
+    });
+    $(function() {
+        var owl = $("#testimonials-slider");
+        owl.owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            nav: true,
+            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+        });
+    });
+    $(function() {
+        var owl = $("#program-slider");
+        owl.owlCarousel({
+            items: 4,
+            loop: true,
+            autoplay: true,
+            stagePadding: 80,
+            margin:20,
+            dots: true,
+            nav: true,
+            smartSpeed: 3000,
+            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items: 1,
+                    nav: false,
+                    stagePadding:0,
+                },
+                576:{
+                    items: 2,
+                },
+                768:{
+                    items: 2,
+                },
+                992:{
+                    items: 3,
+                },
+                1200:{
+                    items: 4,
+                },
+            }
+        });
+    });
+    $(function() {
+        var owl = $("#partner-slider");
+        owl.owlCarousel({
+            items: 3,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            nav: true,
+            smartSpeed: 3000,
+            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items: 1,
+                },
+                576:{
+                    items: 2,
+                },
+                768:{
+                    items: 3,
+                },
+            }
+        });
+    });
+    $(function() {
+        var owl = $("#portfolio-slider");
+        owl.owlCarousel({
+            items: 3,
+            margin:20,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            nav: true,
+            smartSpeed: 3000,
+            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+            responsiveClass:true,
+            responsive:{
+                0:{
+                    items: 1,
+                },
+                576:{
+                    items: 2,
+                },
+                768:{
+                    items: 3,
+                    nav: false,
+                },
+            }
+        });
+    });
+    $(function() {
+        var owl = $("#menu-slider");
+        owl.owlCarousel({
+            items: 1,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            nav: true,
+            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
+        });
+    });
+});
