52cdb9998d6e239b13ed68a21bda35d050439284
Ali-Raza24 <razamughaln@gmail.com>
2024-08-12 14:01:36 +0500

    Latest Code Update

--- src/js/function.js
+++ src/js/function.js
@@ -1,153 +1,53 @@
-jQuery(function($) {
-    "use strict";
-    AOS.init({
-        duration: 1200,
-    })
-    $(function($) {
-        $(".menu-btn").on('click', function() {
-            $("body").toggleClass("menu-active");
-        });
+jQuery(function ($) {
+	"use strict";
+
+	/* Back to top
+    ============================================== */
+	$(window).on('scroll', function () {
+		if ($(document).scrollTop() > 0) {
+			$('#main_header').addClass("nav-fixed");
+		} else {
+			$('#main_header').removeAttr('class');
+		}
+	});
+	/* Back to top
+    ============================================== */
+    var amountScrolled = 700;
+    var backBtn = $("a.back-to");
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            backBtn.fadeIn("slow");
+        } else {
+            backBtn.fadeOut("slow");
+        }
     });
-    $(function() {
-        var owl = $("#main-slider");
-        owl.owlCarousel({
-            items: 1,
-            loop: true,
-            nav: false,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            dots: true,
-            animateOut: 'fadeOut',
-            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
-            responsiveClass:true,
-            responsive:{
-                992:{
-                    nav: true,
-                },
-            }
-        });
+    backBtn.on("click", function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 700);
+        return false;
     });
-    $(function() {
-        var owl = $("#testimonials-slider");
-        owl.owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            dots: true,
-            nav: false,
-            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
-            responsiveClass:true,
-            responsive:{
-                1200:{
-                    nav: true,
-                },
-            }                
-        });
-    });
-    $(function() {
-        var owl = $("#program-slider");
-        owl.owlCarousel({
-            items: 4,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            stagePadding: 80,
-            margin:20,
-            dots: true,
-            nav: true,
-            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items: 1,
-                    stagePadding:0,
-                    nav: false,
-                },
-                576:{
-                    items: 2,
-                },
-                768:{
-                    items: 2,
-                },
-                992:{
-                    items: 3,
-                },
-                1200:{
-                    items: 4,
-                    nav: true,
-                },
-            }
-        });
-    });
-    $(function() {
-        var owl = $("#partner-slider");
-        owl.owlCarousel({
-            items: 3,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            dots: true,
-            nav: true,
-            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items: 1,
-                },
-                576:{
-                    items: 2,
-                },
-                768:{
-                    items: 3,
-                },
-            }
-        });
-    });
-    $(function() {
-        var owl = $("#portfolio-slider");
-        owl.owlCarousel({
-            items: 3,
-            margin:20,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            dots: true,
-            nav: false,
-            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items: 1,
-                },
-                576:{
-                    items: 2,
-                },
-                768:{
-                    items: 3,
-                },
-                1200:{
-                    nav: true,
-                },
-            }
-        });
-    });
-    $(function() {
-        var owl = $("#menu-slider");
-        owl.owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            dots: true,
-            nav: false,
-            navText: ['<span class="fas fa-chevron-left"></span>','<span class="fas fa-chevron-right"></span>'],
-            responsiveClass:true,
-            responsive:{
-                1200:{
-                    nav: true,
-                },
-            }
-        });
-    });
+});
+function playVideo() {
+	const video = document.getElementById("video");
+	const thumbnail = document.querySelector(".thumbnail");
+	const playBtn = document.querySelector(".play-btn");
+
+	thumbnail.style.display = 'none'; // Hide thumbnail
+	playBtn.style.display = 'none'; // Hide play button
+	video.style.display = 'block'; // Show video
+	video.controls = true; // Hide all controls except play button
+	video.play(); // Start video playback
+
+	video.addEventListener('click', function() {
+		if (video.paused) {
+			video.play();
+		} else {
+			video.pause();
+		}
+	});
+}
+document.getElementById('fileInput').addEventListener('change', function() {
+	var fileName = document.getElementById('fileName');
+	fileName.textContent = this.files[0] ? this.files[0].name : 'No file chosen';
 });
